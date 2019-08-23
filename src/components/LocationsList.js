f3212829c7431b36fd25ecea2c59844a6678cdca
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-08-23 13:53:58 -0400

    MVP Met

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,25 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import LocationCard from './LocationCard';
 
-export default function LocationsList() {}
+export default function LocationsList() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/location/`)
+      .then(res => {
+        // console.log(res.data.results);
+        setLocations(res.data.results);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <section className="location-list grid-view">
+      {locations.map(location => {
+        return <LocationCard key={location.id} location={location} />;
+      })}
+    </section>
+  )
+}
